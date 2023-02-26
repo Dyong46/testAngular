84e21b0259f0447e2fe552a22454c17d9039a26d
D u o n g <96007994+Dyong46@users.noreply.github.com>
2023-02-26 14:31:41 +0700

    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DanhsachsanphamComponent } from './danhsachsanpham/danhsachsanpham.component';
+import { ChitietsanphamComponent } from './chitietsanpham/chitietsanpham.component';
+import { DangkyComponent } from './dangky/dangky.component';
+import { LienheComponent } from './lienhe/lienhe.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { DangnhapComponent } from './dangnhap/dangnhap.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, DanhsachsanphamComponent, ChitietsanphamComponent, DangkyComponent, LienheComponent, NotfoundComponent, DangnhapComponent, NavComponent, HeaderComponent, FooterComponent, CarouselComponent, CheckoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/assets/database/Products.js
@@ -0,0 +1,75 @@
+[
+  {
+    "name": "Ritzy Ape Billionaire #27215",
+    "price": "0.0048 ETH",
+    "description": "Last sale: 0.006 ETH",
+    "image": "https://i.seadn.io/gae/EiOaRYhiw_Mwndw8FucNErVkIlyuG30CT0qYtRIMtSxbRviXdYWFd4pb-HqkXGNTe0GUCflnKCWbbhFPlrANknR3Qbyz_HecbvAysHo?auto=format&w=1000"
+  },
+  {
+    "name": "Ritzy Ape Billionaire #19073",
+    "price": "0.0049 ETH",
+    "description": "Last sale: 0.007 ETH",
+    "image": "https://i.seadn.io/gae/USDlmuvxDyBBjwTxynPdPj1zhrKcGvmyzz7ETD_8nWevm73HmuSMSWS9EpO8Z8xa4XysonG3R2z561fjPDdD6s0-9LonRQNR98xI7w?auto=format&w=1000"
+  },
+  {
+    "name": "Ritzy Ape Billionaire #3821",
+    "price": "0.0049 ETH",
+    "description": "Last sale: 0.007 ETH",
+    "image": "https://i.seadn.io/gae/BAjloa0YPwuSdIzlKGer0qaVpXNzT2O6fk_4mxOtIWKa_vIVehsdo8oLzgT9lUp1BzZ10Ft6NdvCuwWcsZhbmrd0IH-piSZ2VLubqQ?auto=format&w=1000"
+  },
+  {
+    "name": "Ritzy Ape Billionaire #40",
+    "price": "0.005 ETH",
+    "description": "Ends in 1 day",
+    "image": "https://i.seadn.io/gae/j3DNAjsMQtmRTutbrG-RngwcB3OvDSgBUJ7DUM91PXG37aGBM6uRx0IfvfumKxRh6PM6osRlRTSCxZJGV18Tu-pRSUhkZ97Da460eQ?auto=format&w=1000"
+  },
+  {
+    "name": "Ritzy Ape Billionaire #29912",
+    "price": "10,000 ETH",
+    "description": "Last sale: 0.005 ETH",
+    "image": "https://i.seadn.io/gae/MXjtWQa-PgDEgFxxxgMUe2YznytEUyyiNjw9jOOnRw5IRRCLlGlpmu30xvrjOIeo6GrJK0dcGKHMEFbqYPyvy2HioucaDnxiuRg7JF4?auto=format&w=1000"
+  },
+  {
+    "name": "Ritzy Ape Billionaire #19073",
+    "price": "777 ETH",
+    "description": "Last sale: 0.009 ETH",
+    "image": "https://i.seadn.io/gae/2lN4CYIPwCq5U47MYX1-wMhgZPGoCNB_tP8-OOwP_PNsC5Y8wWw44ZZ8eZ-ZOdQqFreDYcihcCyTJubCGyxA1qDqyucbN7BwkFOnXg?auto=format&w=1000"
+  },
+  {
+    "name": "Ritzy Ape Billionaire #17154",
+    "price": "100 ETH",
+    "description": "Last sale: 0.009 ETH",
+    "image": "https://i.seadn.io/gae/qt1KhIp3p7yrzRSiI_3Las8da7fzdWlIdZaItJ8m4hj5gb8XqdsYgU8gczk5bQ6a7B36siu1P0m2nr2oEnm4hN-S9UX-YC72NOfblw?auto=format&w=1000"
+  },
+  {
+    "name": "Ritzy Ape Billionaire #40",
+    "price": "13 ETH",
+    "description": "Last sale: 0.009 ETH",
+    "image": "https://i.seadn.io/gae/QVZuVk8BSeVts4y0s-cYDWXoi2Fu0FfG-A3f7NJShF4-aUnge-R1MahJeLPXrqlhgkFPR5d8Ud11i7IqnUEpYPpRu7e4OTlHs5hFwQ?auto=format&w=1000"
+  },
+  {
+    "name": "LUPPY Bsr#3050",
+    "price": "28.8 ETH",
+    "description": "Ends in 6 days",
+    "image": "https://i.seadn.io/gae/qkgnwOZta58eD4wy1qZA1EVgl9pYLmtr0xKNRf9tFf4jpAYg7DDrXDaDPv1KeZI96w1lKAw6b2yMOSVo9gaT7Ls6-h7_JwBkMfF-Rks?auto=format&w=1000"
+  },
+  {
+    "name": "LUPPY Dc#7619",
+    "price": "0.71 ETH",
+    "description": "Ends in 1 days",
+    "image": "https://i.seadn.io/gae/6Dgx7XBWzSidgJzPTkb6N1rB248W8Jnh_zjyv3EV1wa6sditM2aFgIohaZ0N6gVAOC2ns9qC_WUnvzyY3RppH3ImLZqmMp5GofNDQw?auto=format&w=1000"
+  },
+  {
+    "name": "Angel",
+    "price": "2.1681 WETH",
+    "description": "Last sale: 30 ETH",
+    "image": "https://i.seadn.io/gcs/files/ecf45a6daef42643f8746dfebe4c5fa5.png?auto=format&w=1000"
+  },
+  {
+    "name": "Kanpai Panda",
+    "price": "9 ETH",
+    "description": "Last sale: 3.777 ETH",
+    "image": "https://i.seadn.io/gcs/files/90951d0880eaa2fd8adb264a978fcd2e.png?auto=format&w=1000"
+  },
+
+]
